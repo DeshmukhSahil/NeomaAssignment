c1fd2bb8c7308fd60d92539d422f045679b56b08
Sahil Deshmukh <>
2023-12-21 23:58:08 +0530

    update

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const auth = require('../middlewares/auth');
 const Task = require('../models/Task');
 
+router.get('/test', auth, (req, res) => {
+  res.json({
+    message: 'Task routes are working!',
+    user: req.user,
+  });
+});
 
 // CRUD tasks for authenticated users
 
