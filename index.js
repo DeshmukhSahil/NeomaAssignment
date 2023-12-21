855e1dfd4d93fd16c588c11b5955a8b682dfc5f6
Sahil Deshmukh <>
2023-12-21 23:23:34 +0530

    task routes added successfully

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 require('dotenv').config();
 require('./db');
@@ -9,7 +10,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
-
+app.use('/tasks', taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
